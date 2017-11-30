ccae4b2c85d020ef8b6bbf21d80de1290cc7289f
irvingbennett <irving@alairelibre.net>
2017-11-30 12:28:03 -0500

    First try

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 var mongodb = require('mongodb');
 var movies = require('./movies');
-var mongoclient = require('./connect')
+var mongoclient = require('./connect');
+
+var client = mongoclient();
+
+function initMovies(err, db) {
+    if (err) {
+        return console.dir(err);
+    }
+    var collection = db.collection('movies');
+    collection.insert(movies);
+
+}
